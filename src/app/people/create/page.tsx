beea998d73baf0965b1aabaad5ec15ca322505dc
Pablo <lord.pablo125@gmail.com>
2025-02-25 11:31:02 -0300

    deleted files

--- src/app/people/create/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import FormEmployee from '@/components/EmployeeForm'
-import React from 'react'
-
-const CreateEmployee = () => {
-  return <FormEmployee title={'New Employee'} />
-}
-
-export default CreateEmployee
