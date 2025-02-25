ccf4a404fe7684bd41ed90ccbdc96337271a5f95
Pablo <lord.pablo125@gmail.com>
2025-02-25 11:33:47 -0300

    People list, search person & clean unused code

--- src/app/people/components/PeopleList.tsx
+++ src/app/people/components/PeopleList.tsx
@@ -3,48 +3,52 @@
 import {
   Box,
   Button,
-  Pagination,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
   Typography
 } from '@mui/material'
 import Link from 'next/link'
 import { FC, useState } from 'react'
-import AddIcon from '@mui/icons-material/Add'
-import DeleteIcon from '@mui/icons-material/Delete'
 import EditNoteIcon from '@mui/icons-material/EditNote'
-// import { useDeleteEmployee } from '../api'
 
-const PeopleList: FC<T> = ({ people }) => {
-  const [page, setPage] = useState(1)
-  // const { mutate: deleteEmployee } = useDeleteEmployee({
-  //   onSuccess: () => window.location.reload()
-  // })
+const PeopleList: FC<T> = ({ people, isLoadingPeople, errorLoading }) => {
+  const [search, setSearch] = useState('')
+  const filteredPeople = people?.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  )
 
-  const handlePageChange = (
-    event: React.ChangeEvent<unknown>,
-    value: number
-  ) => {
-    setPage(value)
+  if (isLoadingPeople) {
+    return <>Loading...</>
+  }
+
+  if (errorLoading) {
+    return <>Error!</>
   }
 
   return (
     <Box className='flex flex-col items-start pl-4 '>
       <TableContainer className='flex flex-col items-center'>
-        <Typography className='text-center' variant='h2'>
-          Employee List
-        </Typography>
-        <Link href='/employees/create' className='ml-auto mr-4' passHref>
-          <Button color='primary' variant='outlined'>
-            <AddIcon />
-            Add new
-          </Button>
-        </Link>
+        <Box className='mb-10'>
+          <Typography className='text-center' variant='h2'>
+            People List
+          </Typography>
+        </Box>
 
+        <Box className='mb-4 w-full'>
+          <TextField
+            label='Search...'
+            name='dni'
+            type='text'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className='w-full p-2 mb-4 border rounded'
+          />
+        </Box>
         <Table aria-label='simple table'>
           <TableHead>
             <TableRow sx={{ '& > th': { fontWeight: 700 } }}>
@@ -57,53 +61,38 @@ const PeopleList: FC<T> = ({ people }) => {
           </TableHead>
           <TableBody>
             {people?.length > 0 &&
-              people.map(({ height, name, gender, birth_year }: any) => (
-                <TableRow
-                  key={name + birth_year}
-                  className='hover:bg-slate-300'
-                >
-                  <TableCell>{name}</TableCell>
-                  <TableCell>{gender}</TableCell>
-                  <TableCell>{height}</TableCell>
-                  <TableCell>{birth_year}</TableCell>
-                  <TableCell>
-                    <Box>
-                      <Link
-                        href={`/employees/edit/${name}`}
-                        className='ml-auto mr-4'
-                        passHref
-                      >
-                        <Button
-                          className='bg-red-500'
-                          color='inherit'
-                          variant='outlined'
+              filteredPeople.map(
+                ({ height, name, gender, birth_year }: any) => (
+                  <TableRow
+                    key={name + birth_year}
+                    className='hover:bg-slate-300'
+                  >
+                    <TableCell>{name}</TableCell>
+                    <TableCell>{gender}</TableCell>
+                    <TableCell>{height}</TableCell>
+                    <TableCell>{birth_year}</TableCell>
+                    <TableCell>
+                      <Box>
+                        <Link
+                          href={`/people/edit/${name}`}
+                          className='ml-auto mr-4'
+                          passHref
                         >
-                          <EditNoteIcon />
-                        </Button>
-                      </Link>
-                      <Button
-                        color='error'
-                        variant='outlined'
-                        // onClick={() => deleteEmployee(documentId)}
-                      >
-                        <DeleteIcon />
-                      </Button>
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              ))}
+                          <Button
+                            className='bg-red-500'
+                            color='inherit'
+                            variant='outlined'
+                          >
+                            <EditNoteIcon />
+                          </Button>
+                        </Link>
+                      </Box>
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
           </TableBody>
         </Table>
-        <Pagination
-          className='my-4'
-          color='primary'
-          count={5}
-          shape='rounded'
-          variant='outlined'
-          // count={data.totalPages}
-          page={page}
-          onChange={handlePageChange}
-        />
       </TableContainer>
     </Box>
   )
